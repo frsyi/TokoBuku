f9c177d050f902fe0dff6f1c672cfba62b36cbc1
Siti Marfungah <siti@gmail.com>
2024-05-28 04:19:02 +0700

    membuat halaman update

---
+++ resources/views/category/edit.blade.php
@@ -0,0 +1,31 @@
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="text-xl font-semibold leading-tight text-gray-800 dark:text-gray-200">
+            {{ __('Category') }}
+        </h2>
+    </x-slot>
+
+    <div class="py-12">
+        <div class="mx-auto max-w-7xl sm:px-6 lg:px-8">
+            <div class="overflow-hidden bg-white shadow-sm dark:bg-gray-800 sm:rounded-lg">
+                <div class="p-6 text-gray-900 dark:text-gray-100">
+                    <form method="post" action="{{ route('category.update', $category) }}" class="">
+                        @csrf
+                        @method('patch')
+
+                        <div class="mb-6">
+                            <x-input-label for="name" :value="__('Name')" />
+                            <x-text-input id="name" name="name" type="text" class="block w-full mt-1" :value="old('name', $category->name)" required autofocus autocomplete="name" />
+                            <x-input-error class="mt-2" :messages="$errors->get('name')" />
+                        </div>
+
+                        <div class="flex items-center gap-4">
+                            <x-primary-button>{{ __('Save') }}</x-primary-button>
+                            <x-cancel-button href="{{ route('category.index') }}" />
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</x-app-layout>
