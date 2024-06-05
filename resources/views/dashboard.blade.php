00f9aabf83f91656de2f2409fda0f39537d0983a
Siti Marfungah <siti@gmail.com>
2024-06-06 04:06:42 +0700

    perbaikan tampilan dashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,16 +1,28 @@
+<head>
+    <!-- Font Awesome -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
+</head>
+
 <x-app-layout>
     <x-slot name="header">
         <h2 class="text-xl font-semibold leading-tight text-gray-800 dark:text-gray-200">
             {{ __('Dashboard') }}
         </h2>
     </x-slot>
-
     <div class="py-12">
         <div class="mx-auto max-w-7xl sm:px-6 lg:px-8">
             <div class="bg-white shadow-sm overflow-y: auto dark:bg-gray-800 sm:rounded-lg" style="overflow-y: auto;">
                 <div class="bg-white">
                     <div class="max-w-2xl px-4 py-16 mx-auto sm:px-6 sm:py-10 lg:max-w-7xl lg:px-8">
-                        <h2 class="text-2xl font-bold tracking-tight text-gray-900">Related Books</h2>
+                        <div class="flex items-center justify-between">
+                            <h2 class="text-2xl font-bold tracking-tight text-gray-900">Related Books</h2>
+                            <form class="flex items-center gap-2" method="GET" action="{{ route('catalogue.index') }}">
+                                <x-text-input id="search" name="search" type="text" class="w-full" placeholder="Search by title, author, publication year, or category" value="{{ request('search') }}" autofocus />
+                                <x-primary-button type="submit">
+                                    {{ __('Search') }}
+                                </x-primary-button>
+                            </form>
+                        </div>
 
                         <div class="grid grid-cols-1 mt-6 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
                             @foreach($related_books as $book)
@@ -41,8 +53,8 @@
                                         </div>
                                     </div>
                                 </div>
-                            @endforeach
                             </div>
+                            @endforeach
                         </div>
                     </div>
                 </div>
