9e3bba7248eb74761c1699acd3191d4a25b33b9e
Fahma Rosyidah <fahmarosyi@gmail.com>
2024-06-07 12:56:35 +0700

    memperbaiki search

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -2,16 +2,31 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Book;
 use Illuminate\Http\Request;
+use App\Models\Book;
 
 class DashboardController extends Controller
 {
-    // Di dalam method dashboard Controller
-public function dashboard()
-{
-    $related_books = Book::inRandomOrder()->get();
-    return view('dashboard', compact('related_books'));
-}
+    public function dashboard(Request $request)
+    {
+        $search = $request->input('search');
 
+        if ($search) {
+            $related_books = Book::where(function ($query) use ($search) {
+                $query->where('title', 'like', '%' . $search . '%')
+                    ->orWhere('author', 'like', '%' . $search . '%')
+                    ->orWhere('publication_year', 'like', '%' . $search . '%')
+                    ->orWhereHas('category', function ($q) use ($search) {
+                        $q->where('name', 'like', '%' . $search . '%');
+                    });
+            })
+                ->orderBy('title')
+                ->paginate(10)
+                ->withQueryString();
+        } else {
+            $related_books = Book::inRandomOrder()->get();
+        }
+
+        return view('dashboard', compact('related_books'));
+    }
 }
