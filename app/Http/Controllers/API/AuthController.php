7e3ce6ea45746052402df89cee53102ab4d40e7b
Siti Marfungah <siti@gmail.com>
2024-06-09 02:38:59 +0700

    menambahkan api untuk register

--- app/Http/Controllers/API/AuthController.php
+++ app/Http/Controllers/API/AuthController.php
@@ -4,6 +4,9 @@ namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
 
 class AuthController extends Controller
 {
@@ -35,4 +38,40 @@ class AuthController extends Controller
             'status' => 'Logout success!'
         ], 200);
     }
+
+
+    public function register(Request $request)
+    {
+        // Validasi data yang diterima dari request
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|string|max:255',
+            'email' => 'required|string|email|max:255|unique:users',
+            'password' => 'required|string|min:8|confirmed',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json([
+                'status' => 'Validation Error',
+                'errors' => $validator->errors()
+            ], 422);
+        }
+
+        // Buat pengguna baru
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+        ]);
+
+        // Buat token akses personal untuk pengguna
+        $token = $user->createToken('Personal Access Token')->plainTextToken;
+
+        return response()->json([
+            'status' => 'Registration success!',
+            'token_type' => 'Bearer',
+            'access_token' => $token,
+            'user' => $user
+        ], 201);
+    }
+
 }
--- routes/api.php
+++ routes/api.php
@@ -18,7 +18,9 @@ use App\Http\Controllers\API\ProfileController;
 |
 */
 
+Route::post('/register', [AuthController::class, 'register'])->name('register');
 Route::post('/login', [AuthController::class, 'login'])->name('login');
+
 Route::middleware('auth:sanctum')->group(function () {
     Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
     Route::get('/profile', [ProfileController::class, 'index'])->name('profile');
