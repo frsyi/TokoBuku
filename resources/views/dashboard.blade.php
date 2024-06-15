76bd831de2d0b51267cca8587d2fb1437dbfe6ec
Fahma Rosyidah <fahmarosyi@gmail.com>
2024-06-15 13:46:00 +0700

    tampilan dashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -21,7 +21,7 @@
 
         <!-- Page Heading -->
         @if (isset($header))
-            <header class="bg-white shadow dark:bg-gray-800">
+            <header class="bg-indigo-500 shadow dark:bg-gray-800">
                 <div class="px-4 py-6 mx-auto max-w-7xl sm:px-6 lg:px-8">
                     {{ $header }}
                 </div>
@@ -32,11 +32,11 @@
         <main>
             <div class="py-12">
                 <div class="mx-auto max-w-7xl sm:px-6 lg:px-8">
-                    <div class="bg-white shadow-sm overflow-y: auto dark:bg-gray-800 sm:rounded-lg" style="overflow-y: auto;">
+                    <div class="overflow-hidden bg-white shadow-sm dark:bg-gray-800 sm:rounded-lg">
                         <div class="px-6 pt-6 mb-5 md:w-1/2 2xl:w-1/3">
                             @if (request('search'))
                                 <h2 class="pb-3 text-xl font-semibold leading-tight text-gray-800 dark:text-gray-200">
-                                    Search results for : {{ request('search') }}
+                                    Search results for: {{ request('search') }}
                                 </h2>
                             @endif
                             <form class="flex items-center gap-2" method="GET" action="{{ route('dashboard') }}">
@@ -46,38 +46,38 @@
                                 </x-primary-button>
                             </form>
                         </div>
-                        <div class="bg-white">
+                        <div class="bg-gray-100 dark:bg-gray-900">
                             <div class="max-w-2xl px-4 py-16 mx-auto sm:px-6 sm:py-10 lg:max-w-7xl lg:px-8">
                                 <div class="flex items-center justify-between">
-                                    <h2 class="text-2xl font-bold tracking-tight text-gray-900">Related Books</h2>
+                                    <h2 class="text-2xl font-bold tracking-tight text-gray-900 dark:text-gray-100">Related Books</h2>
                                 </div>
 
                                 <div class="grid grid-cols-1 mt-6 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
                                     @foreach($related_books as $book)
                                         <div class="col-md-3">
-                                            <div class="h-full card" style="width: 18rem;">
+                                            <div class="h-full p-4 transition duration-300 transform bg-white border border-gray-200 rounded-lg shadow-lg dark:bg-gray-800 dark:border-gray-700 hover:shadow-2xl hover:-translate-y-1" style="width: 18rem;">
                                                 <a href="{{ route('book.show', $book->id) }}">
-                                                    <img src="{{ asset('storage/' . $book->image) }}" class="card-img-top" alt="{{ $book->title }}" style="height: 300px; object-fit: cover;">
+                                                    <img src="{{ asset('storage/' . $book->image) }}" alt="{{ $book->title }}" class="w-full h-auto mb-4 rounded-t-lg" style="object-fit: contain; max-height: 300px;">
                                                 </a>
                                                 <div class="flex flex-col justify-between card-body">
                                                     <div>
-                                                        <h5 class="text-lg font-bold card-title">{{ $book->title }}</h5>
-                                                        <p class="card-text">
+                                                        <h5 class="text-lg font-bold text-gray-800 dark:text-gray-200">{{ $book->title }}</h5>
+                                                        <p class="text-gray-600 dark:text-gray-400">
                                                             @if ($book->category)
                                                                 {{ $book->category->name }}
                                                             @endif
                                                         </p>
-                                                        <p class="card-text">{{ $book->author }}</p>
+                                                        <p class="text-gray-600 dark:text-gray-400">{{ $book->author }}</p>
                                                     </div>
                                                     <div class="mt-4">
-                                                        <p class="font-bold card-text">Rp{{ number_format($book->price, 2) }}</p>
+                                                        <p class="font-bold text-gray-800 dark:text-gray-200">Rp{{ number_format($book->price, 2) }}</p>
                                                         @auth
                                                             @if(auth()->user()->is_admin)
                                                                 <form action="{{ route('order.index', $book->id) }}" method="POST">
                                                                     @csrf
                                                                     <input type="hidden" name="book_id" value="{{ $book->id }}">
                                                                 </form>
-                                                                @else
+                                                            @else
                                                                 <form action="{{ route('order.index', $book->id) }}" method="POST">
                                                                     @csrf
                                                                     <input type="hidden" name="book_id" value="{{ $book->id }}">
@@ -85,11 +85,9 @@
                                                                         <i class="fas fa-shopping-cart"></i>
                                                                     </x-order-button>
                                                                 </form>
-
                                                             @endif
                                                         @endauth
                                                     </div>
-
                                                 </div>
                                             </div>
                                         </div>
@@ -97,7 +95,9 @@
                                 </div>
 
                                 @if (request('search'))
-                                    {{ $related_books->links() }}
+                                    <div class="mt-6">
+                                        {{ $related_books->links() }}
+                                    </div>
                                 @endif
                             </div>
                         </div>
